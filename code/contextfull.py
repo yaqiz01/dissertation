314a0a45ed6f46d3eacce311a5affcfad2b826ba
Yaqi Zhang <yaqi.zhang@sambanovasystems.com>
2020-05-12 16:30:31 -0700

    udpates

--- code/contextfull.py
+++ code/contextfull.py
@@ -1,19 +1,23 @@
-with Context() as ctx:
-    # scalar and vector input streams
-    bufferA = VecInSteram() 
-    bufferB = ScalInSteram()
-    bufferC = VecInSteram()
-    outputD = VecOutSteram()
+# scalar and vector input streams
+bufferA = VecInSteram() 
+bufferB = ScalInSteram()
+bufferC = VecInSteram()
+outputD = VecOutSteram()
 
-    i = Counter(min=0,max=3,stride=1)
-    j = Counter(min=0,max=3,stride=1)
-    chain = CounterChain(i,j)
+i = Counter(min=0,max=3,stride=1)
+j = Counter(min=0,max=3,stride=1)
+chain = CounterChain(i,j)
 
-    a = bufferA.deq(when=j.valid())
-    b = bufferB.deq(when=i.done())
-    c = bufferC.deq(when=j.done())
+a = bufferA.deq(when=j.valid())
+b = bufferB.deq(when=i.done())
+c = bufferC.deq(when=j.done())
 
-    # b is implicitly broadcasted to all 
-    # lanes
-    expr = a * b + c
-    outputD.enq(data=expr, when=j.valid())
+forward(stage=0, dst="PR0", src=c)
+# b is broadcasted to all lanes
+stage(0,"mul",dst="PR1", a, b)
+stage(1,"add",dst="PR2",oprd=["PR0","PR1"])
+forward(2,dst="PR1",src="PR1")
+forward(3,dst="PR1",src="PR1")
+forward(4,dst="PR1",src="PR1")
+forward(5,dst="PR1",src="PR1")
+outputD.enq(data="PR1", when=j.valid())
