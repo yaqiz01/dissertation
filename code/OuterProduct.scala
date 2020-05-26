bcbcbead45bd43a7ad5de1d3110ad71eadd62450
Yaqi Zhang <yaqi.zhang@sambanovasystems.com>
2020-05-25 23:05:18 -0700

    updates

--- code/OuterProduct.scala
+++ code/OuterProduct.scala
@@ -1,6 +1,7 @@
 // Host to accelerator register for scalar input with 
 // user annotated value
-val N = ArgIn[Int]; bound(N) = 1024
+val N = ArgIn[Int]; 
+bound(N) = 1024
 // 1-D DRAM size in N
 val vecA, vecB = DRAM[T](N)
 // 2-D DRAM size in NxN
@@ -11,26 +12,26 @@ val op1, op2, ip:Int = ...
 val tsA, tsB:Int = ...
 
 // Accelerator kernel
-C0: Accel {
-  // C1 is parallelized by op1
-  C1: Foreach(min=0, step=tsA, max=N, par=op1){ i =>
+Accel {
+  // Parallelized by op1
+  Foreach(min=0, step=tsA, max=N, par=op1){ i =>
     // Allocate 1-D scratchpad size in tsA
     val tileA = SRAM[T](tsA)
     // Load range i to i+tsA of vectorA from off- to 
     // on-chip parallelized by ip
-    C2: tileA load vecA(i::i+tsA par ip) 
-    C3: Foreach(min=0, step=tsB, max=N, par=op2) { j =>
+    tileA load vecA(i::i+tsA par ip) 
+    Foreach(min=0, step=tsB, max=N, par=op2) { j =>
       val tileB = SRAM[T](tsB)
-      C4: tileB load vecB(j::j+tsB par ip) 
+      tileB load vecB(j::j+tsB par ip) 
       // 2-D scratchpad
       val tileC = SRAM[T](tsA, tsB)
-      C5: Foreach(min=0, step=1, max=tsA){ ii =>
+      Foreach(min=0, step=1, max=tsA){ ii =>
         Foreach(min=0, step=1, max=tsB, par=ip) { jj =>
           tileC(ii, jj) = tileA(ii) * tileB(jj)
         }
       }
       // Store partial results to DRAM
-      C6: matC(i::i+tsA, j::j+tsB par ip) store tileC
+      matC(i::i+tsA, j::j+tsB par ip) store tileC
     }
   }
 }
