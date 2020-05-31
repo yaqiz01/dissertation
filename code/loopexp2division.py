0c3ab6fe59734d47244c93e8cb4ee285224e0ab1
Yaqi Zhang <yaqi.zhang@sambanovasystems.com>
2020-05-31 14:52:36 -0700

    update

--- code/loopexp2division.py
+++ code/loopexp2division.py
@@ -2,12 +2,12 @@ mem = rand(N)
 tmp = queue()
 # Initialize queue with 
 # 1 dummy element
-backward = queue(init=1)
+token = queue(init=1)
 @concurrent
 for i in range(1, N):
     tmp.enq(mem[i-1] + i)
-    backward.deq()
+    token.deq()
 @concurrent
 for i in range(1, N):
     mem[i] = tmp.deq()
-    backward.enq(0)
+    token.enq(0)
